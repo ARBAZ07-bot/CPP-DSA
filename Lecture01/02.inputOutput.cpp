4a8c1ba5d50dc679675c2514b709128a80917740
Arbaz Hussain <arbaz07hussain@gmail.com>
2026-08-25 23:26:47 +0530

    Remove lecture folders

--- Lecture01/02.inputOutput.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a,b;
-    cout<<"Enter value of a and b :\n";
-    cin>>a>>b;
-    cout<<"SUM : "<<a+b<<endl;
-    return 0;
-}
--- Lecture06/08.lambdaExpression.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    auto add = [](int a, int b) {
-        return a + b;
-    };
-    cout << add(5, 3)<<endl;
-    return 0;
-}
