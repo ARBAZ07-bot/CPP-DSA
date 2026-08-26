a61293cf4c79bd6db4a370f3e683c6d96378b9e7
Arbaz Hussain <arbaz07hussain@gmail.com>
2026-08-27 01:59:08 +0530

    Lecture02: DataTypes and Operators

---
+++ Lecture02/01.typeConversion.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<(10/3)<<endl;
+    cout<<(10/3.0)<<endl;
+    cout<<('A'+2)<<endl;
+    cout<<(int)'A'<<endl;
+    cout<<(double)(2/5)<<endl;
+    cout<<((double)2/5)<<endl;
+    return 0;
+}
